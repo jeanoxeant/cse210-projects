3bb696b56271f3b1a85c8343a8a7f04b7476cba5
jeanoxeant <oxe24001@byui.edu>
2024-09-21 08:48:12 -0400

    Changes prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage:");
+        string  grade = Console.ReadLine();
+        float userGrade = float.Parse(grade);
+        string letter = "";
+        if (userGrade >= 90 && userGrade <= 100)
+        {
+            letter = "A";
+            if (userGrade < 93)
+            {
+                letter = "A-";
+            }
+        }
+        else if (userGrade >= 80 && userGrade < 90)
+        {
+            letter = "B";
+            if (userGrade >= 87)
+            {
+                letter = "B+";
+            }
+            else if (userGrade < 83)
+            {
+                letter = "B-";
+            }
+           
+        }
+        else if (userGrade >= 70 && userGrade < 80)
+        {
+            letter = "C";
+            if (userGrade >= 77)
+            {
+                letter = "C+";
+            }
+            else if (userGrade < 73)
+            {
+                letter = "C-";
+            }
+        }
+        else if (userGrade >= 60 && userGrade < 70)
+        {
+            letter = "D";
+            if (userGrade >= 67)
+            {
+                letter = "D+";
+            }
+            else if (userGrade < 63)
+            {
+                letter = "D-";
+            }
+           
+        }
+        else if (userGrade < 60)
+        {
+            letter = "F";
+            
+        }
+        else 
+        {
+            letter = "N/A";
+        }
+        Console.WriteLine($"Your grade is {userGrade} and you have {letter}.");
+        if (userGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else 
+        {
+            Console.WriteLine("You have failed the class! You need to work harder for the next time.");
+        }
+        
     }
 }
