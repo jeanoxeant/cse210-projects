a5145237ab783c65920f3eb3622f8e05c305438a
jeanoxeant <oxe24001@byui.edu>
2024-09-25 01:07:26 -0400

    Prep#3 Editted

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magic = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int userNumber;
+        int counter = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber > magic)
+            {
+                 Console.WriteLine("Higher");
+            }
+            else if (userNumber < magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            } 
+            counter++;  
+                        
+        }while (userNumber != magic);
+        Console.WriteLine($"You have guessed the magic number: {counter} times."); 
+        Console.WriteLine();
+
+        string userAnswer = "yes";
+
+        while (userAnswer == "yes")
+        {
+            Console.Write("Do you want to play again? ");
+            userAnswer = Console.ReadLine();
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                userNumber = int.Parse(Console.ReadLine());
+
+                if (userNumber > magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userNumber < magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                } 
+                counter++;  
+                            
+            }while (userNumber != magic);
+            Console.WriteLine($"You have guessed the magic number: {counter} times."); 
+            Console.WriteLine();
+        }
+            
+
+            
+                   
     }
 }
