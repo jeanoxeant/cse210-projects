5d2567ba20937273a989cb1dfc02760a0785b4ac
jeanoxeant <oxe24001@byui.edu>
2024-09-28 21:46:03 -0400

    Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string nombre = PromptUserName();
+        int numero = PromptUserNumber();
+        int raiz = SquareNumber(numero);
+        DisplayResult(nombre, raiz); 
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to my program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Type your number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        return userNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
