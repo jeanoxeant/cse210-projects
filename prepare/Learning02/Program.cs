20f543b61b515007f3323e28388798e89ea7988d
jeanoxeant <oxe24001@byui.edu>
2024-10-30 22:41:34 -0400

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello Learning02 World!");  
+
+        public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+
+        public Job
+        {
+            Job job1 = new Job();
+            job1._jobTitle = "Sofware Engineer";
+        }
+
     }
+    }
+    
 }
